f0698df995a3c85f7d4d75566c28a1537f85d0c6
xh <x3iii131@gmail.com>
2024-06-08 23:47:59 +0100

    更新脱去装备的功能

--- DQQ/DQQ.Api/Services/Itemservices/ServerItemService.cs
+++ DQQ/DQQ.Api/Services/Itemservices/ServerItemService.cs
@@ -13,221 +13,232 @@ using DQQ.Profiles.Items;
 
 namespace DQQ.Api.Services.Itemservices
 {
-  public class ServerItemService : IItemService
-  {
-    private readonly IContext context;
-    private readonly ITemporaryService tiService;
+	public class ServerItemService : IItemService
+	{
+		private readonly IContext context;
+		private readonly ITemporaryService tiService;
 
-    public ServerItemService(IContext context, ITemporaryService tiService)
-    {
-      this.context = context;
-      this.tiService = tiService;
-    }
+		public ServerItemService(IContext context, ITemporaryService tiService)
+		{
+			this.context = context;
+			this.tiService = tiService;
+		}
 
-    public async Task<IEnumerable<ItemEntity>?> ActorInventory(Guid? actorId)
-    {
-      await Task.CompletedTask;
-      if (actorId == null)
-      {
-        return null;
-      }
-      return await context.Query<ItemEntity>(true).Where(b => b.ActorId == actorId && b.IsEquipped != true).WithTranslations().ToArrayAsync();
-    }
+		public async Task<IEnumerable<ItemEntity>?> ActorInventory(Guid? actorId)
+		{
+			await Task.CompletedTask;
+			if (actorId == null)
+			{
+				return null;
+			}
+			return await context.Query<ItemEntity>(true).Where(b => b.ActorId == actorId && b.IsEquipped != true).WithTranslations().ToArrayAsync();
+		}
 
-    public async Task<ContentResponse<bool>> EquipItem(Guid? actorId, Guid? itemId, EnumEquipSlot? slot)
-    {
-      var result = new ContentResponse<bool>();
-      try
-      {
-        if (actorId == null || itemId == null)
-        {
-          return result;
-        }
-        var item = await context.Query<ItemEntity>(false).Where(b => b.Id == itemId && b.ActorId == actorId).FirstOrDefaultAsync();
-        if (item == null || item.EquipType == null)
-        {
-          result.SetError(System.Net.HttpStatusCode.NotFound);
-          return result;
-        }
-        var avaliableSlots = item.EquipType.GetAvaliableSlots();
-        EnumEquipSlot equipSlot;
-        if (slot == null)
-        {
-          equipSlot = avaliableSlots.FirstOrDefault();
-        }
-        else
-        {
-          if (avaliableSlots?.Contains(slot.Value) != true)
-          {
-            result.SetError(System.Net.HttpStatusCode.NotFound);
-            return result;
-          }
-          equipSlot = slot.Value;
-        }
+		public async Task<ContentResponse<bool>> EquipItem(Guid? actorId, Guid? itemId, EnumEquipSlot? slot)
+		{
+			var result = new ContentResponse<bool>();
+			try
+			{
+				if (actorId == null || itemId == null)
+				{
+					return result;
+				}
+				var item = await context.Query<ItemEntity>(false).Where(b => b.Id == itemId && b.ActorId == actorId).FirstOrDefaultAsync();
+				if (item == null || item.EquipType == null)
+				{
+					result.SetError(System.Net.HttpStatusCode.NotFound);
+					return result;
+				}
+				var avaliableSlots = item.EquipType.GetAvaliableSlots();
+				EnumEquipSlot equipSlot;
+				if (slot == null)
+				{
+					equipSlot = avaliableSlots.FirstOrDefault();
+				}
+				else
+				{
+					if (avaliableSlots?.Contains(slot.Value) != true)
+					{
+						result.SetError(System.Net.HttpStatusCode.NotFound);
+						return result;
+					}
+					equipSlot = slot.Value;
+				}
 
 
 
-        if (item.EquipType == EnumEquipType.TwoHandWeapon)
-        {
-          if (item.EquipProfile?.ItemType == EnumItemType.Bows)
-          {
-            var offHandItem = await context.Query<ActorEquipmentEntity>(true).Where(b => b.ActorId == actorId && b.EquipSlot == EnumEquipSlot.OffHand).FirstOrDefaultAsync();
-            if (offHandItem != null && offHandItem?.Item?.EquipProfile?.ItemType == EnumItemType.Quiver)
-            {
-              await UnEquipItem(actorId.Value, EnumEquipSlot.MainHand);
-            }
-            else
-            {
-              await UnEquipItem(actorId.Value, EnumEquipSlot.MainHand, EnumEquipSlot.OffHand);
-            }
-          }
-          else
-          {
-            await UnEquipItem(actorId.Value, EnumEquipSlot.MainHand, EnumEquipSlot.OffHand);
+				if (item.EquipType == EnumEquipType.TwoHandWeapon)
+				{
+					if (item.EquipProfile?.ItemType == EnumItemType.Bows)
+					{
+						var offHandItem = await context.Query<ActorEquipmentEntity>(true).Where(b => b.ActorId == actorId && b.EquipSlot == EnumEquipSlot.OffHand).FirstOrDefaultAsync();
+						if (offHandItem != null && offHandItem?.Item?.EquipProfile?.ItemType == EnumItemType.Quiver)
+						{
+							await unEquipItem(false, actorId.Value, EnumEquipSlot.MainHand);
+						}
+						else
+						{
+							await unEquipItem(false, actorId.Value, EnumEquipSlot.MainHand, EnumEquipSlot.OffHand);
+						}
+					}
+					else
+					{
+						await unEquipItem(false, actorId.Value, EnumEquipSlot.MainHand, EnumEquipSlot.OffHand);
 
-          }
+					}
 
-        }
-        else
-        {
-          if (equipSlot == EnumEquipSlot.OffHand)
-          {
-            var mainHand = await context.Query<ActorEquipmentEntity>(true).Where(b => b.ActorId == actorId && b.EquipSlot == EnumEquipSlot.MainHand).FirstOrDefaultAsync();
-            if (mainHand != null && mainHand.Item?.EquipProfile?.EquipType == EnumEquipType.TwoHandWeapon)
-            {
-              if (!(item.EquipProfile?.ItemType == EnumItemType.Quiver && mainHand.Item?.EquipProfile.ItemType == EnumItemType.Bows))
-              {
-                await UnEquipItem(actorId.Value, EnumEquipSlot.MainHand);
-              }
-            }
-          }
-          await UnEquipItem(actorId.Value, equipSlot);
-        }
-        var newEquip = new ActorEquipmentEntity()
-        {
-          ActorId = actorId,
-          ItemId = itemId,
-          EquipSlot = equipSlot,
-        };
-        await context.AddAsync(newEquip);
-        await context.SaveChangesAsync();
-        result.SetSuccess(true);
+				}
+				else
+				{
+					if (equipSlot == EnumEquipSlot.OffHand)
+					{
+						var mainHand = await context.Query<ActorEquipmentEntity>(true).Where(b => b.ActorId == actorId && b.EquipSlot == EnumEquipSlot.MainHand).FirstOrDefaultAsync();
+						if (mainHand != null && mainHand.Item?.EquipProfile?.EquipType == EnumEquipType.TwoHandWeapon)
+						{
+							if (!(item.EquipProfile?.ItemType == EnumItemType.Quiver && mainHand.Item?.EquipProfile.ItemType == EnumItemType.Bows))
+							{
+								await unEquipItem(false, actorId.Value, EnumEquipSlot.MainHand);
+							}
+						}
+					}
+					await unEquipItem(false, actorId.Value, equipSlot);
+				}
+				await context.SaveChangesAsync();
+				var newEquip = new ActorEquipmentEntity()
+				{
+					ActorId = actorId,
+					ItemId = itemId,
+					EquipSlot = equipSlot,
+				};
+				await context.AddAsync(newEquip);
+				await context.SaveChangesAsync();
+				result.SetSuccess(true);
 
-      }
-      catch (Exception ex)
-      {
-        result.SetError(ex);
-      }
+			}
+			catch (Exception ex)
+			{
+				result.SetError(ex);
+			}
 
-      return result;
-    }
+			return result;
+		}
 
-    public async Task<IEnumerable<ItemEntity>?> PickableItems(Guid? actorId)
-    {
-      if (actorId == null)
-      {
-        return null;
-      }
-      var items = await tiService.GetAllTemporaryItems(actorId);
-      var itemIds = items.Select(b => b.Id).ToHashSet();
-      var itemsAlreadyExisting = await context.Query<ItemEntity>(true).Where(b => itemIds.Contains(b.Id)).Select(b => b.Id).ToArrayAsync();
-      var itemNeedRemove = items.Where(b => itemsAlreadyExisting.Contains(b.Id)).Select(b => b.Id).ToArray();
-      await tiService.PickAndRemoveTemporaryItems(actorId, itemNeedRemove);
-      return items.Where(b => !itemsAlreadyExisting.Contains(b.Id)).ToArray();
-    }
+		public async Task<IEnumerable<ItemEntity>?> PickableItems(Guid? actorId)
+		{
+			if (actorId == null)
+			{
+				return null;
+			}
+			var items = await tiService.GetAllTemporaryItems(actorId);
+			var itemIds = items.Select(b => b.Id).ToHashSet();
+			var itemsAlreadyExisting = await context.Query<ItemEntity>(true).Where(b => itemIds.Contains(b.Id)).Select(b => b.Id).ToArrayAsync();
+			var itemNeedRemove = items.Where(b => itemsAlreadyExisting.Contains(b.Id)).Select(b => b.Id).ToArray();
+			await tiService.PickAndRemoveTemporaryItems(actorId, itemNeedRemove);
+			return items.Where(b => !itemsAlreadyExisting.Contains(b.Id)).ToArray();
+		}
 
-    public async Task<ContentResponse<bool>> PickItem(Guid? actorId, params Guid[] itemId)
-    {
-      var result = new ContentResponse<bool>();
-      if (actorId == null || itemId?.Any() != true)
-      {
-        return result;
-      }
-      var foundItem = await context.Query<ItemEntity>(true).Where(b => itemId.Any(i => b.Id == i)).AnyAsync();
-      if (foundItem)
-      {
-        result.SetValidation(ValidationResultHelper.New("One of Item already picked"));
-        return result;
+		public async Task<ContentResponse<bool>> PickItem(Guid? actorId, params Guid[] itemId)
+		{
+			var result = new ContentResponse<bool>();
+			if (actorId == null || itemId?.Any() != true)
+			{
+				return result;
+			}
+			var foundItem = await context.Query<ItemEntity>(true).Where(b => itemId.Any(i => b.Id == i)).AnyAsync();
+			if (foundItem)
+			{
+				result.SetValidation(ValidationResultHelper.New("One of Item already picked"));
+				return result;
 
-      }
-      var items = await tiService.PickAndRemoveTemporaryItems(actorId, itemId);
-      if (items?.Any() == null)
-      {
-        result.SetError(System.Net.HttpStatusCode.NotFound);
-        return result;
-      }
+			}
+			var items = await tiService.PickAndRemoveTemporaryItems(actorId, itemId);
+			if (items?.Any() == null)
+			{
+				result.SetError(System.Net.HttpStatusCode.NotFound);
+				return result;
+			}
 
-      foreach (var item in items.GroupBy(b => b.ItemNumber))
-      {
-        var itemProfile = DQQPool.TryGet<ItemProfile, EnumItem?>(item.Key);
-        if (itemProfile == null)
-        {
-          continue;
-        }
-        if (itemProfile.IsStack)
-        {
-          var sum = item.Sum(b => b.Quantity ?? 0);
-          if (sum == 0)
-          {
-            continue;
-          }
-          var existingItem = await context.Query<ItemEntity>(false).Where(b => b.ActorId == actorId && b.ItemNumber == item.Key).FirstOrDefaultAsync();
-          if (existingItem != null)
-          {
-            existingItem.Quantity = existingItem.Quantity + sum;
-          }
-          else
-          {
-            var first = item.FirstOrDefault();
-            first.Quantity = sum;
-            first.ActorId = actorId;
-            await context.AddAsync(first);
-          }
-        }
-        else
-        {
-          foreach (var subItem in item)
-          {
-            subItem.ActorId = actorId;
-            await context.AddAsync(subItem);
-          }
-        }
-      }
-      try
-      {
-        await context.SaveChangesAsync();
-        result.SetSuccess(true);
-      }
-      catch (Exception ex)
-      {
-        result.SetError(ex);
+			foreach (var item in items.GroupBy(b => b.ItemNumber))
+			{
+				var itemProfile = DQQPool.TryGet<ItemProfile, EnumItem?>(item.Key);
+				if (itemProfile == null)
+				{
+					continue;
+				}
+				if (itemProfile.IsStack)
+				{
+					var sum = item.Sum(b => b.Quantity ?? 0);
+					if (sum == 0)
+					{
+						continue;
+					}
+					var existingItem = await context.Query<ItemEntity>(false).Where(b => b.ActorId == actorId && b.ItemNumber == item.Key).FirstOrDefaultAsync();
+					if (existingItem != null)
+					{
+						existingItem.Quantity = existingItem.Quantity + sum;
+					}
+					else
+					{
+						var first = item.FirstOrDefault();
+						first.Quantity = sum;
+						first.ActorId = actorId;
+						await context.AddAsync(first);
+					}
+				}
+				else
+				{
+					foreach (var subItem in item)
+					{
+						subItem.ActorId = actorId;
+						await context.AddAsync(subItem);
+					}
+				}
+			}
+			try
+			{
+				await context.SaveChangesAsync();
+				result.SetSuccess(true);
+			}
+			catch (Exception ex)
+			{
+				result.SetError(ex);
 
-      }
-      return result;
-    }
-    public async Task<ContentResponse<bool>> UnEquipItem(Guid? actorId, params EnumEquipSlot[] slots)
-    {
-      var result = new ContentResponse<bool>();
+			}
+			return result;
+		}
+		public async Task<ContentResponse<bool>> UnEquipItem(Guid? actorId, params EnumEquipSlot[] slots)
+		{
+			return await unEquipItem(true, actorId, slots);
+		}
+		private async Task<ContentResponse<bool>> unEquipItem(bool save, Guid? actorId, params EnumEquipSlot[] slots)
+		{
+			var result = new ContentResponse<bool>();
 
-      if (actorId == null || slots?.Any() != true)
-      {
-        result.SetSuccess(true);
-        return result;
-      }
+			if (actorId == null || slots?.Any() != true)
+			{
+				result.SetSuccess(true);
+				return result;
+			}
 
-      var list = slots.ToArray();
-      var existingEquip = await context.Query<ActorEquipmentEntity>(false).Where(b =>
-            b.ActorId == actorId && (b.EquipSlot == null || slots.Contains(b.EquipSlot.Value)))
-          .ToArrayAsync();
-      foreach (var e in existingEquip)
-      {
-        context.Delete<ActorEquipmentEntity>(e);
-      }
-      await context.SaveChangesAsync();
-      result.SetSuccess(true);
+			var list = slots.ToArray();
+			var existingEquip = await context.Query<ActorEquipmentEntity>(false).Where(b =>
+						b.ActorId == actorId && (b.EquipSlot == null || slots.Contains(b.EquipSlot.Value)))
+					.ToArrayAsync();
+			foreach (var e in existingEquip)
+			{
+				context.Delete<ActorEquipmentEntity>(e);
+			}
+			if (save)
+			{
+				await context.SaveChangesAsync();
+				result.SetSuccess(true);
+			}
+			else
+			{
+				result.SetSuccess(true);
+			}
 
-      return result;
-    }
-
-  }
+			return result;
+		}
+	}
 }
