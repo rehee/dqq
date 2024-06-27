c674f10f665bc7901eb91e305b345f4ae0f394bb
xh <x3iii131@gmail.com>
2024-06-27 15:45:12 +0100

    重新添加了home控制器

---
+++ DQQ/DQQ.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DQQ.Api.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return Content("1");
+		}
+	}
+}
